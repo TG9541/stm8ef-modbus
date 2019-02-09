a5eb22efc636cc8c7bfc4a8ef7ca6bf1205d6ede
Thomas <thomas.goeppel@gmail.com>
2019-02-09 13:29:54 +0100

    move init to main.fs

--- main.fs
+++ main.fs
@@ -1,1 +1,16 @@
-#include MBSERVER
+#require MBSERVER
+
+RAM
+
+  2 CONSTANT BAUD9600
+
+NVM
+  \ --- MODBUS server startup
+
+  : init ( -- )
+    BAUD9600 UARTISR
+    MBSERVER
+  ;
+
+  ' init    'BOOT !
+RAM
